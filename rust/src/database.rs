9b55ba401c5a072ffe28ef0a1a9c91e375f33db1
Elchin Gasimov <egasimov0@gmail.com>
2025-09-25 16:08:16 +0400

    Cleanup stub files

--- rust/src/database.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: Copyright The LanceDB Authors
-
-// Re-export database functionality from connection module
-// This module serves as a compatibility layer
-
-pub use crate::connection::*;
