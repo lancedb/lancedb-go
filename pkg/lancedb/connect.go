b94ad9909e37d6a23dd4ec857aee2c484dc82510
Elchin Gasimov <egasimov0@gmail.com>
2025-09-30 11:51:44 +0400

    switch to dynamic CGO

--- pkg/lancedb/connect.go
+++ pkg/lancedb/connect.go
@@ -3,12 +3,6 @@ package lancedb
 //go:generate go run ../../cmd/download-binaries
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/../../include
-#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/../../lib/darwin_amd64/liblancedb_go.a -framework Security -framework CoreFoundation
-#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/../../lib/darwin_arm64/liblancedb_go.a -framework Security -framework CoreFoundation
-#cgo linux,amd64 LDFLAGS: ${SRCDIR}/../../lib/linux_amd64/liblancedb_go.a -lm -ldl -lpthread
-#cgo linux,arm64 LDFLAGS: ${SRCDIR}/../../lib/linux_arm64/liblancedb_go.a -lm -ldl -lpthread
-#cgo windows,amd64 LDFLAGS: ${SRCDIR}/../../lib/windows_amd64/liblancedb_go.a
 #include "lancedb.h"
 */
 import "C"
